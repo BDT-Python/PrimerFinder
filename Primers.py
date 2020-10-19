56ad11f6ba217a176368e8ab06ae0446a5c00d34
mirthelc <73112838+mirthelc@users.noreply.github.com>
2020-10-19 15:17:02 +0200

    Update Primers.py

--- Primers.py
+++ Primers.py
@@ -1,5 +1,33 @@
 print('Enter your sequence: ')
-seq = input()
+seq = input(gcggcgcgcc tgggcgctaa gatggcggcg gcgtgagttg catgttgtgt gaggatcccg
+gggccgccgc gtcgctcggg ccccgcc atg gccgtcacca tcacgctcaa aacgctgcag
+cagcagacct tcaagatccg catggagcct gacgagacgg tgaaggtgct aaaggagaag
+atagaagctg agaagggtcg tgatgccttc cccgtggctg gacagaaact catctatgcc
+ggcaagatct tgagtgacga tgtccctatc agggactatc gcatcgatga gaagaacttt
+gtggtcgtca tggtgaccaa gaccaaagcc ggccagggta cctcagcacc cccagaggcctcacccacag ctgccccaga gtcctctaca tccttcccgc ctgcccccac ctcaggcatg
+tcccatcccc cacctgccgc cagagaggac aagagcccat cagaggaatc cgcccccacg
+    acgtccccag agtctgtgtc aggctctgtt ccctcttcag gtagcagcgg gcgagaggaa
+    gacgcggcct ccacgctagt gacgggctct gagtatgaga cgatgctgac ggagatcatg
+    tccatgggct atgagcgaga gcgggtcgtg gccgccctga gagccagcta caacaacccc
+    caccgagccg tggagtatct gctcacggga attcctggga gccccgagcc ggaacacggt
+     tctgtccagg agagccaggt atcggagcag ccggccacgg aagcagcagg agagaacccc
+    ctggagttcc tgcgggacca gccccagttc cagaacatgc ggcaggtgat tcagcagaac
+    cctgcgctgc tgcccgccct gctccagcag ctgggccagg agaaccctca gcttttacag
+    caaatcagcc ggcaccagga gcagttcatc cagatgctga acgagccccc tggggagctg
+    gcggacatct cagatgtgga gggggaggtg ggcgccatag gagaggaggc cccgcagatg
+    aactacatcc aggtgacgcc gcaggagaaa gaagctatag agaggttgaa ggccctgggc
+    ttcccagaga gcctggtcat ccaggcctat ttcgcgtgtg aaaaaaatga gaacttggct
+    gccaacttcc tcctgagtca gaactttgat gacgagtgat gccaggaagc caggccaccg
+    aagcccccac cctaccctta ttccatgaaa gttttataaa agaaaaaata tatatatatt
+    catgtttatt taagaaatgg aaaaaaaaat caaaaatctt aaaaaaacaa gcaaacagtc
+    cagcttcctg tcctcctaaa gtggcccctg ttcccatctc ccgggccaga cagctgtccc
+    cccgtcctcc tccccagccc agcctgctca gagaagctgg caggactggg aggcgacaga
+     tgggcccctc ttggcctctg tcccagctct ctgcagccag acggaaaggc ggctgcttgc
+     ctctccatcc tccgaaaaac ccctgaggac ccccccccat cctcttctag gatgagggga
+     agctggagcc ccaactttga tcctccattg gagtggccca aatctttcca tctagggcaa
+     gtcctgaaag gcccaaggcc ccctccccag tctggccttg gcctccagcc tggagaaggg
+     ctaacatcag ctcattgtca aggccacccc caccccagaa cagaaccgtg tctctgataa
+     aggttttgaa gtgaataaag ttttaaaaac ta)
 
 print('How many nucleotides should the primer be?')
 length = input()
