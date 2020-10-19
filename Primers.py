cceaab1d8bd7e0ec6ca7acdcd1c31f8cc4311d99
STHolst <68391640+STHolst@users.noreply.github.com>
2020-10-19 15:46:27 +0200

    Update Primers.py

--- Primers.py
+++ Primers.py
@@ -1,38 +1,8 @@
-print('Enter your sequence: ')
-seq = input(gcggcgcgcc tgggcgctaa gatggcggcg gcgtgagttg catgttgtgt gaggatcccg
-gggccgccgc gtcgctcggg ccccgcc atg gccgtcacca tcacgctcaa aacgctgcag
-cagcagacct tcaagatccg catggagcct gacgagacgg tgaaggtgct aaaggagaag
-atagaagctg agaagggtcg tgatgccttc cccgtggctg gacagaaact catctatgcc
-ggcaagatct tgagtgacga tgtccctatc agggactatc gcatcgatga gaagaacttt
-gtggtcgtca tggtgaccaa gaccaaagcc ggccagggta cctcagcacc cccagaggcctcacccacag ctgccccaga gtcctctaca tccttcccgc ctgcccccac ctcaggcatg
-tcccatcccc cacctgccgc cagagaggac aagagcccat cagaggaatc cgcccccacg
-    acgtccccag agtctgtgtc aggctctgtt ccctcttcag gtagcagcgg gcgagaggaa
-    gacgcggcct ccacgctagt gacgggctct gagtatgaga cgatgctgac ggagatcatg
-    tccatgggct atgagcgaga gcgggtcgtg gccgccctga gagccagcta caacaacccc
-    caccgagccg tggagtatct gctcacggga attcctggga gccccgagcc ggaacacggt
-     tctgtccagg agagccaggt atcggagcag ccggccacgg aagcagcagg agagaacccc
-    ctggagttcc tgcgggacca gccccagttc cagaacatgc ggcaggtgat tcagcagaac
-    cctgcgctgc tgcccgccct gctccagcag ctgggccagg agaaccctca gcttttacag
-    caaatcagcc ggcaccagga gcagttcatc cagatgctga acgagccccc tggggagctg
-    gcggacatct cagatgtgga gggggaggtg ggcgccatag gagaggaggc cccgcagatg
-    aactacatcc aggtgacgcc gcaggagaaa gaagctatag agaggttgaa ggccctgggc
-    ttcccagaga gcctggtcat ccaggcctat ttcgcgtgtg aaaaaaatga gaacttggct
-    gccaacttcc tcctgagtca gaactttgat gacgagtgat gccaggaagc caggccaccg
-    aagcccccac cctaccctta ttccatgaaa gttttataaa agaaaaaata tatatatatt
-    catgtttatt taagaaatgg aaaaaaaaat caaaaatctt aaaaaaacaa gcaaacagtc
-    cagcttcctg tcctcctaaa gtggcccctg ttcccatctc ccgggccaga cagctgtccc
-    cccgtcctcc tccccagccc agcctgctca gagaagctgg caggactggg aggcgacaga
-     tgggcccctc ttggcctctg tcccagctct ctgcagccag acggaaaggc ggctgcttgc
-     ctctccatcc tccgaaaaac ccctgaggac ccccccccat cctcttctag gatgagggga
-     agctggagcc ccaactttga tcctccattg gagtggccca aatctttcca tctagggcaa
-     gtcctgaaag gcccaaggcc ccctccccag tctggccttg gcctccagcc tggagaaggg
-     ctaacatcag ctcattgtca aggccacccc caccccagaa cagaaccgtg tctctgataa
-     aggttttgaa gtgaataaag ttttaaaaac ta)
+seq = "gcggcgcgcc tgggcgctaa gatggcggcg gcgtgagttg catgttgtgt gaggatcccggggccgccgc gtcgctcggg ccccgcc atg gccgtcacca tcacgctcaa aacgctgcagcagcagacct tcaagatccg catggagcct gacgagacgg tgaaggtgct aaaggagaagatagaagctg agaagggtcg tgatgccttc cccgtggctg gacagaaact catctatgccggcaagatct tgagtgacga tgtccctatc agggactatc gcatcgatga gaagaactttgtggtcgtca tggtgaccaa gaccaaagcc ggccagggta cctcagcacc cccagaggcctcacccacag ctgccccaga gtcctctaca tccttcccgc ctgcccccac ctcaggcatgtcccatcccc cacctgccgc cagagaggac aagagcccat cagaggaatc cgcccccacgacgtccccag agtctgtgtc aggctctgtt ccctcttcag gtagcagcgg gcgagaggaagacgcggcct ccacgctagt gacgggctct gagtatgaga cgatgctgac ggagatcatgtccatgggct atgagcgaga gcgggtcgtg gccgccctga gagccagcta caacaacccccaccgagccg tggagtatct gctcacggga attcctggga gccccgagcc ggaacacggttctgtccagg agagccaggt atcggagcag ccggccacgg aagcagcagg agagaaccccctggagttcc tgcgggacca gccccagttc cagaacatgc ggcaggtgat tcagcagaaccctgcgctgc tgcccgccct gctccagcag ctgggccagg agaaccctca gcttttacagcaaatcagcc ggcaccagga gcagttcatc cagatgctga acgagccccc tggggagctggcggacatct cagatgtgga gggggaggtg ggcgccatag gagaggaggc cccgcagatgaactacatcc aggtgacgcc gcaggagaaa gaagctatag agaggttgaa ggccctgggcttcccagaga gcctggtcat ccaggcctat ttcgcgtgtg aaaaaaatga gaacttggctgccaacttcc tcctgagtca gaactttgat gacgagtgat gccaggaagc caggccaccgaagcccccac cctaccctta ttccatgaaa gttttataaa agaaaaaata tatatatattcatgtttatt taagaaatgg aaaaaaaaat caaaaatctt aaaaaaacaa gcaaacagtccagcttcctg tcctcctaaa gtggcccctg ttcccatctc ccgggccaga cagctgtccccccgtcctcc tccccagccc agcctgctca gagaagctgg caggactggg aggcgacagatgggcccctc ttggcctctg tcccagctct ctgcagccag acggaaaggc ggctgcttgcctctccatcc tccgaaaaac ccctgaggac ccccccccat cctcttctag gatgaggggaagctggagcc ccaactttga tcctccattg gagtggccca aatctttcca tctagggcaagtcctgaaag gcccaaggcc ccctccccag tctggccttg gcctccagcc tggagaagggctaacatcag ctcattgtca aggccacccc caccccagaa cagaaccgtg tctctgataaaggttttgaa gtgaataaag ttttaaaaac ta"
+seq.replace(" ", "")
+seq.upper()
+print(seq)
 
-print('How many nucleotides should the primer be?')
-length = input()
-
-print(seq + ' ' + length)
 
 #for i in range(len(data1)):
     #if data1[i] == data2[i]:
