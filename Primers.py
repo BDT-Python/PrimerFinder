6c635476a5ccb68bf4806b564171ec835228d4d2
STHolst <s.t.holst95@gmail.com>
2020-10-26 17:21:18 +0100

    i am lost and hopeless

--- Primers.py
+++ Primers.py
@@ -8,6 +8,10 @@ primer_min_len = 15
 primer_max_len = 19
 seq_len = len(noSpace)
 
+
+
+
+
 # Defines primer_dict as a list to hold libraries with potential primer information.
 primer_dict = []
 for i in range(seq_len):
@@ -33,32 +37,32 @@ for i in range(seq_len):
                  "End": nucleotideCounter + len(primerSeq),
                  "Length:": len(primerSeq),
                  "Sequence:": primerSeq,
-                 "GC%": percentage,
-                 "Tm": tm}
+                 "GC%": (round(percentage, 2)),
+                 "Tm": (round(tm, 1)), }
             primer_dict.append(d)                           # Appends and stores each primer dictionary in the list
 
-print(primer_dict)
+# Finding the primer pairs in the primer dictionary (a reverse that matches the forward) which creates a product of minimum 30nt and has a Tm difference of 5 or less 
+min_distance = 30
+pairs = []
 
 
 def make_reverse(toReverse):               # Function to fetch the reverse compliment of the reverse primer.
     normal = "acgt"
     compliment = "tgca"
     reversePrimer = str.maketrans(normal, compliment)
-    print(toReverse.translate(reversePrimer)[::-1])
+    primer_dict[x]["Sequence:"] = toReverse.translate(reversePrimer)[::-1]
     return
 
 
-make_reverse(noSpace)  # Demonstration of the make_reverse function in use. Prints reverse compliment of the entire sequence.
+for i in range(len(primer_dict)):
+    options = []
+    for x in range(i, (len(primer_dict))):
+        if abs(primer_dict[i]["Tm"] - primer_dict[x]["Tm"]) <= 5:
+            if primer_dict[x]["Begin"] - primer_dict[i]["End"] >= min_distance:
+                make_reverse(primer_dict[x]["Sequence:"])
+                options.append(primer_dict[x])
+    if options:
+        pairs.append([primer_dict[i], options])
 
+print(pairs)
 
-# We have every single suitable primer saved into primer_dict.
-# Now the following tasks remain:
-#
-#   1. Request product length from user and fetch two primers, roughly the desired length apart
-#      This should be relatively easy, considering the primer start/end nt are saved in primer_dict.
-#        https://medium.com/@rob3hr/searching-through-a-list-of-dictionaries-in-python-618fe77b2799
-#
-#   2. Check for self-annealing. This one's a bit more tricky but I'm sure we'll figure it out
-#       Maybe, we can get it to superimpose the primers form every possible position and
-#       add a +1 for every base that can connect and set a cutoff value.
-#       (i.e. if score = 6 (6 bases pair) at any position, the primer is considered self-annealing.
