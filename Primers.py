71a66eaa351824f4ac84179055585a3bd5571a15
STHolst <s.t.holst95@gmail.com>
2020-10-27 14:54:37 +0100

    "1.0"

--- Primers.py
+++ Primers.py
@@ -1,12 +1,12 @@
 # Processes input. First makes it lowercase, then removes the spaces to get a pure sequence.
-rawSequence = "gcggcgcgcc tgggcgctaa gatggcggcg gcgtgagttg catgttgtgt gaggatcccggggccgccgc gtcgctcggg ccccgcc atg gccgtcacca tcacgctcaa aacgctgcagcagcagacct tcaagatccg catggagcct gacgagacgg tgaaggtgct aaaggagaagatagaagctg agaagggtcg tgatgccttc cccgtggctg gacagaaact catctatgccggcaagatct tgagtgacga tgtccctatc agggactatc gcatcgatga gaagaactttgtggtcgtca tggtgaccaa gaccaaagcc ggccagggta cctcagcacc cccagaggcctcacccacag ctgccccaga gtcctctaca tccttcccgc ctgcccccac ctcaggcatgtcccatcccc cacctgccgc cagagaggac aagagcccat cagaggaatc cgcccccacgacgtccccag agtctgtgtc aggctctgtt ccctcttcag gtagcagcgg gcgagaggaagacgcggcct ccacgctagt gacgggctct gagtatgaga cgatgctgac ggagatcatgtccatgggct atgagcgaga gcgggtcgtg gccgccctga gagccagcta caacaacccccaccgagccg tggagtatct gctcacggga attcctggga gccccgagcc ggaacacggttctgtccagg agagccaggt atcggagcag ccggccacgg aagcagcagg agagaaccccctggagttcc tgcgggacca gccccagttc cagaacatgc ggcaggtgat tcagcagaaccctgcgctgc tgcccgccct gctccagcag ctgggccagg agaaccctca gcttttacagcaaatcagcc ggcaccagga gcagttcatc cagatgctga acgagccccc tggggagctggcggacatct cagatgtgga gggggaggtg ggcgccatag gagaggaggc cccgcagatgaactacatcc aggtgacgcc gcaggagaaa gaagctatag agaggttgaa ggccctgggcttcccagaga gcctggtcat ccaggcctat ttcgcgtgtg aaaaaaatga gaacttggctgccaacttcc tcctgagtca gaactttgat gacgagtgat gccaggaagc caggccaccgaagcccccac cctaccctta ttccatgaaa gttttataaa agaaaaaata tatatatattcatgtttatt taagaaatgg aaaaaaaaat caaaaatctt aaaaaaacaa gcaaacagtccagcttcctg tcctcctaaa gtggcccctg ttcccatctc ccgggccaga cagctgtccccccgtcctcc tccccagccc agcctgctca gagaagctgg caggactggg aggcgacagatgggcccctc ttggcctctg tcccagctct ctgcagccag acggaaaggc ggctgcttgcctctccatcc tccgaaaaac ccctgaggac ccccccccat cctcttctag gatgaggggaagctggagcc ccaactttga tcctccattg gagtggccca aatctttcca tctagggcaagtcctgaaag gcccaaggcc ccctccccag tctggccttg gcctccagcc tggagaagggctaacatcag ctcattgtca aggccacccc caccccagaa cagaaccgtg tctctgataaaggttttgaa gtgaataaag ttttaaaaac ta"
+rawSequence = input("Please enter your sequence: ")
 lowerSequence = rawSequence.lower()
 noSpace = rawSequence.replace(" ", "")
 
 nucleotideCounter = 0
-primer_min_len = 28
-primer_max_len = 30
-seq_len = len(noSpace)
+primer_min_len = 28     # Defines minimum primer length
+primer_max_len = 30     # Defines maximum primer length
+seq_len = len(noSpace)  # Stores length of the processed sequence as an integer.
 
 
 def make_reverse(toReverse):               # Function to fetch the reverse compliment of the reverse primer.
@@ -58,24 +58,25 @@ min_distance = 100
 pairs = []
 cnt = 0
 
-f = open("output.txt", "w")     # Opens output file
+f = open("output.txt", "w")                         # Opens output file
 i = 0
 for primer1 in primer_dict:
-    for primer2 in primer_dict[i:]:
-        if abs( primer1["Tm"] - primer2["Tm"]) <= 5:
+    for primer2 in primer_dict[i:]:                 # Ensures it doesn't check for reverse primers behind the fwd.
+        if abs( primer1["Tm"] - primer2["Tm"]) <= 5 and (primer1["Sequence:"].endswith("g") or primer1["Sequence:"].endswith("c")):
             if primer2["Begin"] - primer1["End"] >= min_distance:
-                temp = primer2
-                temp["Sequence:"] = make_reverse(temp["Sequence:"])
-                f.write("Pair #: " + str(cnt) + "\n")
-                f.write("Forward Primer: " + str(primer1) + "\n")
-                f.write("Reverse Primer: " + str(temp) + "\n")
-                f.write("Product Length: " + str(primer2["End"] - primer1["Begin"]) + "\n")
-                f.write("\n")
-                pairs.append([primer1, primer2])
-                cnt = cnt + 1                       # Counts valid pairs
+                if primer2["Sequence:"].startswith("g") or primer2["Sequence:"].startswith("c"):
+                    temp = primer2
+                    temp["Sequence:"] = make_reverse(temp["Sequence:"])
+                    f.write("Pair #: " + str(cnt) + "\n")
+                    f.write("Forward Primer: " + str(primer1) + "\n")
+                    f.write("Reverse Primer: " + str(temp) + "\n")
+                    f.write("Product Length: " + str(primer2["End"] - primer1["Begin"]) + "\n")
+                    f.write("\n")
+                    pairs.append([primer1, primer2])
+                    cnt = cnt + 1                       # Counts valid pairs
     i = i+1
 
-f.close()                       # Closes output file
+f.close()                                           # Closes output file
 
 num = 0
 
